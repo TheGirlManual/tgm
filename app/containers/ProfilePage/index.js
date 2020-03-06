5920a2b2c96b27daca3b07a62ca209243ddaaf86
OFRBG <ofonsk@gmail.com>
2020-03-05 19:24:51 -0600

    feat: add responsive for mobile and edge cases

--- app/containers/ProfilePage/index.js
+++ app/containers/ProfilePage/index.js
@@ -26,28 +26,32 @@ export function ProfilePage() {
       height="80vh"
     >
       <Box width={0.9} flex="1" bg="primary">
-        <Heading color="black" sx={{ fontSize: [120, 160] }}>
+        <Heading color="black" sx={{ fontSize: ['8vw'] }}>
           Essi Kiiski
         </Heading>
-        <Heading color="#333" sx={{ fontSize: [30, 36] }}>
+        <Heading color="#333" sx={{ fontSize: ['4vw'] }}>
           Political Scientist
         </Heading>
-        <Text color="#333" sx={{ fontSize: [20, 30] }}>
+        <Text color="#333" sx={{ fontSize: ['4vw'] }}>
           Brussels
         </Text>
       </Box>
-      <Box
+      <Flex
         width={0.9}
         flex="1"
         bg="secondary"
+        alignItems="flex-end"
         sx={{ position: 'relative' }}
         mt={3}
-        p={5}
+        p={3}
       >
         <Text
+          sx={{ position: 'relative', zIndex: 3 }}
+          mb={3}
+          ml={3}
           color="primary"
-          width="60%"
-          fontSize={[18, 20, 22, 28]}
+          width="70%"
+          fontSize={['2vw', '1.8vw']}
           lineHeight={1.8}
         >
           Essi Kiiski is a Bachelor of Political Science and Master in Violin
@@ -59,14 +63,15 @@ export function ProfilePage() {
         </Text>
         <Image
           sx={{
+            zIndex: 2,
             position: 'absolute',
             bottom: 0,
             right: 0,
-            zoom: 1.1,
+            zoom: 0.8,
           }}
           src="https://firebasestorage.googleapis.com/v0/b/interactive-coolture.appspot.com/o/assets%2Fimg%2Fprofile-photos%2Fessi-kiiski-nobg.png?alt=media&token=55eeb710-a285-4efb-a248-18aa78ad1d3f"
         />
-      </Box>
+      </Flex>
     </Flex>
   );
 }
